9da81ac58d7dd96ba4ac906e3d912bb485b4125e
Badr Elbadry <badr.ahmed.2015@gmail.com>
2023-02-23 14:39:50 +0300

    modifying the first java project

--- project1.java
+++ project1.java
@@ -2,5 +2,8 @@ public class project1{
           public static void main(String[] args) {
                     System.out.println("hello world");
                     System.out.println("hello africa");
+                    System.out.println("hello Egypt");
+                    System.out.println("hello Assiut");
+
           }
 } 
